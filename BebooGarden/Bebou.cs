ee69046805c8c2ce845b4a7512ef585bd77f9fdc
Saladeuh <emilienmonet10@gmail.com>
2024-05-11 10:09:30 +0200

    change project name de Beboo Garden

---
+++ BebooGarden/Bebou.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chaos;
+
+internal class Bebou
+{
+  public string Name { get; set; }
+  public int energy { get; set; }
+  public int Happiness { get; set; }
+  public Mood mood { get; set; }
+  public Vector3 position { get; set; }
+}
+
+internal enum Mood
+{
+  Happy,
+  Angry,
+  Sad,
+}
