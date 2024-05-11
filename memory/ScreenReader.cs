051ceb146242ba3a32b9234646fa5354f56dc104
Saladeuh <emilienmonet10@gmail.com>
2024-05-11 05:47:25 +0200

    add screen reader support with tolk

---
+++ memory/ScreenReader.cs
@@ -0,0 +1,47 @@
+using DavyKager;
+using System;
+using System.IO;
+
+namespace FFXIVAccess;
+
+internal class ScreenReader
+{
+  internal static bool Output(string text, bool interrupt = false)
+  {
+    if (text.Length == 0)
+    {
+      return false;
+    }
+
+    var success = Tolk.Output(text, interrupt);
+    return success;
+  }
+
+  internal static void Load()
+  {
+    // Append accessibility deps (e.g. Tolk, NVDA drivers, etc.) to PATH
+    var path = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+    var accessibilityAssembliesDir = Path.Combine("Libs");
+    path += $";{accessibilityAssembliesDir}";
+    Environment.SetEnvironmentVariable("PATH", path, EnvironmentVariableTarget.Process);
+    
+    // Load Tolk
+    Tolk.TrySAPI(true);
+    Tolk.Load();
+  }
+
+  internal static void Unload()
+  {
+    Tolk.Unload();
+  }
+
+  internal static bool IsUsingSAPI()
+  {
+    return Tolk.DetectScreenReader().Equals("SAPI");
+  }
+
+  internal static void Interrupt()
+  {
+    Tolk.Output("", true);
+  }
+}
