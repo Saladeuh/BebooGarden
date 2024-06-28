dc97c9f652fd60dcb2c87b6ea18defafddde924e
Saladeuh <emilienmonet10@gmail.com>
2024-06-28 13:48:27 +0200

    fancy move behaviour

--- FormChao/GameCore/Beboo.cs
+++ FormChao/GameCore/Beboo.cs
@@ -20,33 +20,43 @@ internal class Beboo
     Mood = Mood.Happy;
     Position = new Vector3(0, 0, 0);
     SoundSystem = soundSystem;
-    BebooBehaviour cuteBehaviour = new BebooBehaviour(this, 5000, 20000, (Beboo beboo) =>
+    BebooBehaviour cuteBehaviour = new(this, 5000, 20000, (Beboo beboo) =>
     {
       beboo.DoCuteThing();
     });
     cuteBehaviour.Start();
-    BebooBehaviour moveBehaviour = new BebooBehaviour(this, 200, 400, (Beboo beboo) =>
+    BebooBehaviour moveBehaviour = new(this, 200, 400, (Beboo beboo) =>
     {
       beboo.MoveTowardGoal();
     });
     moveBehaviour.Start();
+    BebooBehaviour fancyMoveBehaviour = new(this, 30000, 60000, (Beboo beboo) =>
+    {
+      beboo.WannaGoToRandomPlace();
+    });
+    fancyMoveBehaviour.Start();
   }
   public bool MoveTowardGoal()
   {
-    if (GoalPosition == null || GoalPosition==Position) return false;
-    Vector3 direction = (Vector3) GoalPosition - Position;
+    if (GoalPosition == null || GoalPosition == Position) return false;
+    Vector3 direction = (Vector3)GoalPosition - Position;
     Vector3 directionNormalized = Vector3.Normalize(direction);
-    directionNormalized.X=Math.Sign(directionNormalized.X);
+    directionNormalized.X = Math.Sign(directionNormalized.X);
     directionNormalized.Y = Math.Sign(directionNormalized.Y);
     Position += directionNormalized;
-    ScreenReader.Output($"{Position.X} {Position.Y}");
-    bool moved =Position != GoalPosition;
-    if (moved) SoundSystem.PlayBebooSound(SoundSystem.BebooStepSound, this);
-    else GoalPosition = null;
+    bool moved = Position != GoalPosition;
+    SoundSystem.PlayBebooSound(SoundSystem.BebooStepSound, this);
+    if(!moved) GoalPosition = null;
     return moved;
   }
   public void DoCuteThing()
   {
     SoundSystem.PlayBebooSound(SoundSystem.BebooCuteSounds, this);
+  }
+  public void WannaGoToRandomPlace()
+  {
+    var rnd = new Random();
+    var randomMove = new Vector3(rnd.Next(-4, 5), rnd.Next(-4, 5), 0);
+    GoalPosition = Position + randomMove;
   }
 }
