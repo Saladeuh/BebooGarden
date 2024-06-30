d31a37908425ac6ffa9713890ef9b69863e09860
Saladeuh <emilienmonet10@gmail.com>
2024-07-01 00:05:21 +0200

    save process ended

--- FormChao/SoundSystem.cs
+++ FormChao/SoundSystem.cs
@@ -17,6 +17,7 @@ internal class SoundSystem
   public Sound WhistleSound { get; set; }
   public List<Sound> BebooSleepingSounds { get; private set; }
   public Sound BebooStepSound { get; private set; }
+  public Sound GrassSound { get; private set; }
 
   private static System.Timers.Timer AmbiTimer;
 
@@ -25,7 +26,7 @@ internal class SoundSystem
     //Creates the FmodSystem object
     System = FmodAudio.Fmod.CreateSystem();
     //System object Initialization
-    System.Init(32, InitFlags._3D_RightHanded | InitFlags.Vol0_Becomes_Virtual);
+    System.Init(4095, InitFlags._3D_RightHanded | InitFlags.Vol0_Becomes_Virtual);
     var advancedSettings = new AdvancedSettings();
     advancedSettings.Vol0VirtualVol = 0.01f;
     System.SetAdvancedSettings(advancedSettings);
@@ -45,24 +46,11 @@ internal class SoundSystem
       sounds.Add(sound);
     }
   }
-  private void onAmbiTimer(object? sender, ElapsedEventArgs e)
-  {
-    var rand = new Random();
-    var sound = AmbiSounds[rand.Next(AmbiSounds.Count())];
-    Channel channel = System.PlaySound(sound, paused: false);
-    channel.Set3DAttributes(new Vector3(rand.Next(-20, 20), rand.Next(-20, 20), 5f), default, default);
-    channel.Set3DMinMaxDistance(0, 35);
-    AmbiTimer.Dispose();
-    AmbiTimer = new System.Timers.Timer(rand.Next(4000, 8000));
-    AmbiTimer.Elapsed += onAmbiTimer;
-    AmbiTimer.Enabled = true;
-  }
-
   public void LoadMainScreen()
   {
     Sound sound;
-    sound = sound = System.CreateStream(CONTENTFOLDER + "music/neutral.mp3", Mode.Loop_Normal);
-    Channel channel = (Channel?)System.PlaySound(sound, paused: false);
+    sound = System.CreateStream(CONTENTFOLDER + "music/neutral.mp3", Mode.Loop_Normal);
+    Channel channel = (Channel?)System.PlaySound(sound, paused: true);
     channel.SetLoopPoints(TimeUnit.MS, 12, TimeUnit.MS, 88369);
     channel.Volume = 0.5f;
     sound = System.CreateStream(CONTENTFOLDER + "sounds/WaterCalmWide.wav", Mode.Loop_Normal | Mode._3D | Mode._3D_InverseTaperedRolloff);
@@ -85,6 +73,7 @@ internal class SoundSystem
     LoadSoundsInList(["ronfle.wav", "dodo.wav"], BebooSleepingSounds, "sounds/beboo/");
     WhistleSound = System.CreateSound(CONTENTFOLDER + "sounds/character/se_sys_whistle_1p.wav", Mode._3D | Mode._3D_LinearSquareRolloff | Mode.Unique);
     BebooStepSound = System.CreateSound(CONTENTFOLDER + "sounds/beboo/step.wav", Mode._3D | Mode._3D_LinearSquareRolloff | Mode.Unique);
+    GrassSound = System.CreateSound(CONTENTFOLDER + "sounds/grass_rustle.wav", Mode._3D | Mode._3D_LinearSquareRolloff | Mode.Unique);
     Reverb3D reverb = System.CreateReverb3D();
     reverb.SetProperties(Preset.Bathroom);
     reverb.Set3DAttributes(new Vector3(0, 0, 0), 0f, 500f);
@@ -97,6 +86,19 @@ internal class SoundSystem
       var sound = System.CreateSound(files[i], Mode._3D | Mode._3D_LinearSquareRolloff);
       AmbiSounds.Add(sound);
     }
+  }
+
+  private void onAmbiTimer(object? sender, ElapsedEventArgs e)
+  {
+    var rand = new Random();
+    var sound = AmbiSounds[rand.Next(AmbiSounds.Count())];
+    Channel channel = System.PlaySound(sound, paused: false);
+    channel.Set3DAttributes(new Vector3(rand.Next(-20, 20), rand.Next(-20, 20), 5f), default, default);
+    channel.Set3DMinMaxDistance(0, 35);
+    AmbiTimer.Dispose();
+    AmbiTimer = new System.Timers.Timer(rand.Next(4000, 8000));
+    AmbiTimer.Elapsed += onAmbiTimer;
+    AmbiTimer.Enabled = true;
   }
 
   public List<Task> tasks = new();
