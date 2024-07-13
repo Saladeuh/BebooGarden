a71f8635dc000d548c3911a2333c3e305ba94450
Saladeuh <emilienmonet10@gmail.com>
2024-07-13 18:50:37 +0200

    add more list textbox related pleasure

--- FormChao/Interface/UI/TextForm.cs
+++ FormChao/Interface/UI/TextForm.cs
@@ -32,8 +32,12 @@ public partial class TextForm : Form
   {
     if (e.KeyCode == Keys.Enter)
     {
+      if (TextBox.Text.Length == 0) { 
+        ScreenReader.Output("non vide");
+        return;
+      }
       Game.SoundSystem.System.PlaySound(Game.SoundSystem.MenuOkSound);
-      Result = TextBox.Text;
+      Result = Char.ToUpper(TextBox.Text[0]) + TextBox.Text[1..];
       Close();
     }
   }
