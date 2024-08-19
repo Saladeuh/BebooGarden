08b0edabb85522382ba7507d1fa57a9702edf99e
Saladeuh <emilienmonet10@gmail.com>
2024-08-19 17:47:10 +0200

    beboo born in water is a swimmer

--- FormChao/GameCore/Item/Egg.cs
+++ FormChao/GameCore/Item/Egg.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing.Drawing2D;
 using System.Numerics;
 using BebooGarden.GameCore.Pet;
+using BebooGarden.GameCore.World;
 using BebooGarden.Interface.ScriptedScene;
 using FmodAudio;
 
@@ -11,12 +12,13 @@ public class Egg(string color) : Item
   protected override string _translateKeyName { get; } = "egg.name";
   protected override string _translateKeyDescription { get; } = "egg.description";
   public override Vector3? Position { get; set; } // position null=in inventory
-  public override bool IsTakable { get; set; } = false;
+  public override bool IsTakable { get; set; } = true;
+  public override bool IsWaterProof { get; set; } = true;
   public override Channel? Channel { get; set; }
   private string Color { get; set; } = color;
 
   public override void Action() => Hatch();
-  public override void Take() => Hatch();
+  //public override void Take() => Hatch();
   public override void BebooAction(Beboo beboo) => Hatch();
   private void Hatch()
   {
@@ -24,7 +26,8 @@ public class Egg(string color) : Item
     SoundLoopTimer.Stop();
     Game.SoundSystem.PlayCinematic(Game.SoundSystem.CinematicHatch);
     var name = NewBeboo.Run();
-    Game.Map?.Beboos.Add(new Beboo(name, 1, DateTime.MinValue));
+    var swimLevel = (Game.Map?.IsInLake(Position ?? new(0, 0, 0)) ?? false) ? 10 : 0;
+    Game.Map?.Beboos.Add(new Beboo(name, 1, DateTime.MinValue, 5, swimLevel, false));
     Game.Flags.NewGame = false;
     Game.UpdateMapMusic();
   }
