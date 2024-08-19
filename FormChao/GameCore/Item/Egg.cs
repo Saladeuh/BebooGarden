25e54f951b426b852985f187604d4a5640b5b882
Saladeuh <emilienmonet10@gmail.com>
2024-08-19 17:49:24 +0200

    Beboo pop where its egg was

--- FormChao/GameCore/Item/Egg.cs
+++ FormChao/GameCore/Item/Egg.cs
@@ -12,13 +12,13 @@ public class Egg(string color) : Item
   protected override string _translateKeyName { get; } = "egg.name";
   protected override string _translateKeyDescription { get; } = "egg.description";
   public override Vector3? Position { get; set; } // position null=in inventory
-  public override bool IsTakable { get; set; } = true;
+  public override bool IsTakable { get; set; } = false;
   public override bool IsWaterProof { get; set; } = true;
   public override Channel? Channel { get; set; }
   private string Color { get; set; } = color;
 
   public override void Action() => Hatch();
-  //public override void Take() => Hatch();
+  public override void Take() => Hatch();
   public override void BebooAction(Beboo beboo) => Hatch();
   private void Hatch()
   {
@@ -27,7 +27,7 @@ public class Egg(string color) : Item
     Game.SoundSystem.PlayCinematic(Game.SoundSystem.CinematicHatch);
     var name = NewBeboo.Run();
     var swimLevel = (Game.Map?.IsInLake(Position ?? new(0, 0, 0)) ?? false) ? 10 : 0;
-    Game.Map?.Beboos.Add(new Beboo(name, 1, DateTime.MinValue, 5, swimLevel, false));
+    Game.Map?.Beboos.Add(new Beboo(name, 1, DateTime.MinValue, 5, swimLevel, false) { Position = this.Position ?? new(0, 0, 0) });
     Game.Flags.NewGame = false;
     Game.UpdateMapMusic();
   }
